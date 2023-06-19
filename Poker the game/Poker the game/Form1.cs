a7a214835da8cc4e7a9723afd4d32d28585c5380
KratoNius <rankumyt@gmail.com>
2023-06-19 08:02:11 +0300

    хз

--- Poker the game/Poker the game/Form1.cs
+++ Poker the game/Poker the game/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,10 +18,21 @@ namespace Poker_the_game
 		{
 			InitializeComponent();
 		}
-
+		byte[] imageBytes;
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			string imageUrl = "https://github.com/KratoNius/Poker-the-game/blob/main/1670225311_grizly-club-p-shablon-igralnikh-kart-6.jpg";
 
+			using (var client = new WebClient())
+			{
+				imageBytes = client.DownloadData(imageUrl);
+			}
+
+			using (var stream = new MemoryStream(imageBytes))
+			{
+				var image = Image.FromStream(stream);
+				pictureBox1.Image = image;
+			}
 		}
 	}
 }
